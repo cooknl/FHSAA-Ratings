46e10b62de51eb260ee1b6969fc832b5275b87a5
CAPN <nathan.gs20@gmail.com>
2022-04-28 23:06:52 -0500

    updates R ver, removes multi checkboxes, fluidRow layout

--- app.R
+++ app.R
@@ -8,6 +8,7 @@
 #
 
 
+require(stringr)
 require(plotly)
 require(shiny)
 require(ggplot2)
@@ -18,6 +19,9 @@ require(paletteer)
 require(viridis)
 require(gginnards)
 require(lubridate)
+require(shinyjs)
+
+
 
 load("./divisions.RData")
 # load("./OLD-divisions.RData") # only for dev of update buttons
@@ -30,47 +34,46 @@ lax_map <- ggplot(FL, aes(x = long, y = lat)) +
     scale_color_paletteer_c("viridis::inferno",
                             limits = c(floor(min(divisions$Rating)),
                                        ceiling(max(divisions$Rating)))) +
-    theme(axis.title = element_blank()) 
+    theme(axis.title = element_blank())
 
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+    useShinyjs(),
     # Application title
     titlePanel(title = "Florida Girls Lacrosse Ratings",
                windowTitle = "FL GLAX Ratings"),
     
-    
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput(
-                inputId = "ratings",
-                label = "Range of Ratings:",
-                min = floor(min(divisions$Rating)),
-                max = ceiling(max(divisions$Rating)),
-                value = c(floor(min(divisions$Rating)),
-                          ceiling(max(divisions$Rating)))
-            ),
-            checkboxGroupInput(
-                inputId = "divs_check",
-                label = "Divisions:",
-                choices = unique(divisions$Division),
-                selected = unique(divisions$Division)
-            )
+    fluidRow(column(12,
+        # Sidebar with a slider input for number of bins
+        sliderInput(
+            inputId = "ratings",
+            label = "Range of Ratings:",
+            min = floor(min(divisions$Rating)),
+            max = ceiling(max(divisions$Rating)),
+            value = c(floor(min(divisions$Rating)),
+                      ceiling(max(divisions$Rating)))
         ),
-        
-        # Show a plot of the generated distribution
-        mainPanel(plotlyOutput("FLmap"))
+        offset = 8
+    )),
+    fluidRow(column(12,
+                    # Show a plot of the generated distribution
+                    
+                    plotlyOutput("FLmap", width = "100%"),)),
+    
+    fluidRow(
+        column(
+            12,
+            actionButton(inputId = "check_rankings",
+                         label = "Check for Updated Rankings"),
+            
+            actionButton(inputId = "update_rankings",
+                         label = "Update Rankings"),
+            
+            textOutput("dateInfo"),
+            offset = 8
+        )
     ),
-    actionButton(
-        inputId ="check_rankings",
-        label = "Check for Updated Rankings"
-    ),
-    actionButton(
-        inputId ="update_rankings",
-        label = "Update Rankings"
-    ),
-    textOutput("dateInfo"),
     hr(),
     HTML(
         '<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
@@ -79,12 +82,12 @@ ui <- fluidPage(
         <br />
         <span xmlns:dct="http://purl.org/dc/terms/" href="http://purl.org/dc/dcmitype/InteractiveResource" property="dct:title" rel="dct:type">
             Florida Girls Lacrosse Ratings
-        </span> 
-        by 
+        </span>
+        by
         <a xmlns:cc="http://creativecommons.org/ns#" href="https://capn.shinyapps.io/FL-GIrls-LAX-Ratings/" property="cc:attributionName" rel="cc:attributionURL">
             Nathan Cook
-        </a> 
-        is licensed under a 
+        </a>
+        is licensed under a
         <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
             Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
         </a>.'
@@ -92,10 +95,24 @@ ui <- fluidPage(
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output,session) {
-    
+server <- function(input, output, session) {
     # reactiveValues object for storing current data set.
     vals <- reactiveValues(date_check = NULL)
+    
+    observe({
+        shinyjs::hide("update_rankings")
+        if (!is.null(vals$date_check)) {
+            if ((
+                stringr::str_detect(
+                    vals$date_check,
+                    "^A rankings update is available as of"
+                )
+            )) {
+                shinyjs::show("update_rankings")
+            }
+            
+        }
+    })
     
     observeEvent(input$check_rankings, {
         old_rankingsDate <- rankingsDate
@@ -105,7 +122,13 @@ server <- function(input, output,session) {
         load("./divisions.RData")
         print((old_rankingsDate  %--%  rankingsDate) %/% days(1))
         if ((old_rankingsDate  %--%  rankingsDate) %/% days(1)) {
-            vals$date_check = paste("A rankings update is available as of", day(rankingsDate), month(rankingsDate, label=TRUE), year(rankingsDate), sep = " ")
+            vals$date_check = paste(
+                "A rankings update is available as of",
+                day(rankingsDate),
+                month(rankingsDate, label = TRUE),
+                year(rankingsDate),
+                sep = " "
+            )
         }
         else
             vals$date_check = "No update available"
@@ -121,19 +144,21 @@ server <- function(input, output,session) {
         message('reloading')
         source("./get-fhsaa-rankings.R")
         load("./divisions.RData")
-        vals$date_check = paste("Rankings have been updated and are", (rankingsDate %--% as_datetime(today())) %/% days(1), "days old.", sep = " ")
+        vals$date_check = paste(
+            "Rankings have been updated and are",
+            (rankingsDate %--% as_datetime(today())) %/% days(1),
+            "days old.",
+            sep = " "
+        )
     })
     
     
     selected <- reactive({
         (divisions$Rating > input$ratings[[1]]) &
-            (divisions$Rating < input$ratings[[2]]) &
-            (divisions$Division %in% input$divs_check)
+            (divisions$Rating < input$ratings[[2]])
     })
     
     output$FLmap <- renderPlotly({
-        
-        
         FL <- map_data("state", region = "florida")
         
         
@@ -147,20 +172,25 @@ server <- function(input, output,session) {
                            label = SchoolName
                        ))
         
-        ggplotly(lax_map) %>% 
+        ggplotly(lax_map) %>%
             plotly::config(displaylogo = FALSE) %>%
-            plotly::config(modeBarButtonsToRemove = c("select2d",
-                                                      "lasso2d",
-                                                      "autoScale2d",
-                                                      "hoverClosestCartesian",
-                                                      "hoverCompareCartesian")) %>%
-            layout(uirevision = TRUE)
-        
+            plotly::config(
+                modeBarButtonsToRemove = c(
+                    "select2d",
+                    "lasso2d",
+                    "autoScale2d",
+                    "hoverClosestCartesian",
+                    "hoverCompareCartesian"
+                )
+            )
     })
     
     output$dateInfo <- renderText({
         if (is.null(vals$date_check))
-            paste("Rankings are", (rankingsDate %--% as_datetime(today())) %/% days(1), "days old.", sep = " ")
+            paste("Rankings are",
+                  (rankingsDate %--% as_datetime(today())) %/% days(1),
+                  "days old.",
+                  sep = " ")
         else
             vals$date_check
     })
